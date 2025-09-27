3e076bc64f1d5e35bbc9f6abdaacdad91aba9ae5
haritimasinha27 <2022ugcm004@nitjsr.ac.in>
2025-09-27 06:40:50 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ui, name='ui'),
     path('health', views.health, name='health'),
-    path('extract', views.extract, name='extract'),
-    path('normalize', views.normalize, name='normalize'),
     path('process', views.process, name='process'),
 ]
 
